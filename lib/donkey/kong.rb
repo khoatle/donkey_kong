63473d25f9b012adbdd2e011e5cfd3d6af585f60
Robert Boone <robert@rlb3.com>
2013-06-13 07:43:07 -0500

    Convert method added.

--- lib/donkey/kong.rb
+++ lib/donkey/kong.rb
@@ -2,6 +2,19 @@ require "donkey/kong/version"
 
 module Donkey
   module Kong
-    # Your code goes here...
+    def self.convert(number)
+
+      raise NotANumber unless /\d+/.match(number.to_s)
+
+      if number % 15 == 0
+        "DonkeyKong"
+      elsif number % 5 == 0
+        "Kong"
+      elsif number % 3 == 0
+        "Donkey"
+      else
+        number
+      end
+    end
   end
 end
--- spec/donkey/kong_spec.rb
+++ spec/donkey/kong_spec.rb
@@ -12,18 +12,18 @@ describe Donkey::Kong do
                [8, 8],
                [9, "Donkey"],
                [15, "DonkeyKong"],
-               [20, 20],
+               [20, "Kong"],
                [30, "DonkeyKong"],
                [45, "DonkeyKong"],
                [50, "Kong"],
                [70, "Kong"],
-               [75, "Kong"],
+               [75, "DonkeyKong"],
                [90, "DonkeyKong"],
                [100, "Kong"],
               ]
   spec_list.each do |test_set|
     it "#{test_set[0]} should be #{test_set[1]}" do
-      expect(Donkey::Kong.convert(test_set[0])).to be(test_set[0])
+      expect(Donkey::Kong.convert(test_set[0])).to eq(test_set[1])
     end
   end
 end
