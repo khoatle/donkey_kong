ccd4534b8eecf8634a35fbc78daf3a97509cab50
Robert Boone <robert@rlb3.com>
2013-06-13 07:29:42 -0500

    Setup spec.

---
+++ spec/donkey/kong_spec.rb
@@ -0,0 +1,30 @@
+require_relative '../spec_helper.rb'
+require_relative '../../lib/donkey/kong'
+
+describe Donkey::Kong do
+  spec_list = [
+               [1, 1],
+               [2, 2],
+               [3, "Donkey"],
+               [4, 4],
+               [5, "Kong"],
+               [6, "Donkey"],
+               [7, 7],
+               [8, 8],
+               [9, "Donkey"],
+               [15, "DonkeyKong"],
+               [20, 20],
+               [30, "DonkeyKong"],
+               [45, "DonkeyKong"],
+               [50, "Kong"],
+               [70, "Kong"],
+               [75, "Kong"],
+               [90, "DonkeyKong"],
+               [100, "Kong"],
+              ]
+  spec_list.each do |test_set|
+    it "#{test_set[0]} should be #{test_set[1]}" do
+      expect(Donkey::Kong.convert(test_set[0])).to be(test_set[0])
+    end
+  end
+end
